6020af59c7d1c5d0437007c4248673e612a09632
monicajoa <mnortiz.ortiz@gmail.com>
2021-04-26 18:15:39 -0500

    Basic annotations - concat,  type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""[Basic annotations - Concat]
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """[Type-annotated function concat that takes str1 and str2 as arguments]
+
+    Args:
+        str1 (str): [First string]
+        str2 (str): [Second string]
+
+    Returns:
+        str: [Concatenated string]
+    """
+    return str1 + str2
