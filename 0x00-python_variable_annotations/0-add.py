6ce608da094b5f9ebd6e825fa932a1b8d001ed67
monicajoa <mnortiz.ortiz@gmail.com>
2021-04-26 18:10:08 -0500

    Add Basic annotations - addfile, type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""[Basic annotations - Add]
+"""
+
+
+def add(a: float, b: float) -> float:
+    """A type-annotated function add that takes a and b as arguments
+
+    Args:
+        a (float): [First float argument]
+        b (float): [Second float argument]
+
+    Returns:
+        float: [their sum as a float.]
+    """
+    return a + b
