6d66448c50e620b9811561bee8508f95bb60d367
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-04 00:24:03 -0500

    Add LRU Caching task, Create a class LRUCache that inherits from BaseCaching and is a caching system, discard the least recently used item (LRU algorithm), change first conditional, delete else in the first conditional

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -8,7 +8,6 @@ BaseCaching = __import__('base_caching').BaseCaching
 class LRUCache(BaseCaching):
     """[Class that inherits from BaseCaching
     and becomes a caching system]
-
     Args:
         BaseCaching ([Class]): [Parent class]
     """
@@ -23,7 +22,6 @@ class LRUCache(BaseCaching):
         """[Method that assign to the dictionary
         the item value for the key, discard
         the least recently used item (LRU algorithm)]
-
         Args:
                 key: [Key]
                 item: [Value for the key]
@@ -31,9 +29,8 @@ class LRUCache(BaseCaching):
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.key_list.remove(key)
-            else:
-                self.cache_data[key] = item
-                self.key_list.append(key)
+            self.cache_data[key] = item
+            self.key_list.append(key)
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             least_used_key = self.key_list.pop(0)
             print('DISCARD:', least_used_key)
@@ -41,12 +38,13 @@ class LRUCache(BaseCaching):
 
     def get(self, key):
         """[Method that Get an item by key]
-
         Args:
                 key: [Key]
                 Returns: [Value in dictionay linked to key]
         """
         if key is not None and key in self.cache_data:
+            self.key_list.remove(key)
+            self.key_list.append(key)
             return self.cache_data[key]
         else:
             return None
