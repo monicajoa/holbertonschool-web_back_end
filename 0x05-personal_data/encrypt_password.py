a957f20501d7825dc378d3b21e52e804be749f27
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-11 00:12:47 -0500

    Check valid password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -16,3 +16,22 @@ def hash_password(password: str) -> bytes:
         bytes: [Hashed password, which is a byte string]
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password:  bytes, password: str) -> bool:
+    """[Function that expects two arguments,
+    validate that the provided password matches
+    the hashed password]
+
+    Args:
+        hashed_password (bytes)
+        password (str)
+
+    Returns:
+        bool: [Boolean]
+    """
+    new_password = password.encode('utf-8')
+    if bcrypt.checkpw(new_password, hashed_password):
+        return True
+    else:
+        return False
