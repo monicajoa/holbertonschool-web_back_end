1515aaaf53d671d8be4c82bf1577c996dcfe36fb
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-13 23:59:54 -0500

    Add pycodestyle task

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,4 +11,17 @@ class BasicAuth(Auth):
     Args:
         Auth ([Class]): [Parent class]
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """ Returns the Base64 part of the Authorization
+        header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header[:6] == 'Basic ':
+            return None
+        else:
+            return authorization_header[6:]
