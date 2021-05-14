e821488bf1dba147f5f289fd4df08e80006321d1
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-13 23:48:36 -0500

    Add Basic auth task, Create a class BasicAuth that inherits from Auth. For the moment this class will be empty

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""[Basic auth]
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """[Class that inherits from Auth]
+
+    Args:
+        Auth ([Class]): [Parent class]
+    """
+    pass
