76230a4a4ca8b6bab44820403045aa47bb4752fd
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-18 18:57:20 -0500

    Add Basic - Base64 decode task, n the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,21 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a Base64
+        string base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode = base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+            return decode
+        except Exception:
+            return None
+        else:
+            return None
