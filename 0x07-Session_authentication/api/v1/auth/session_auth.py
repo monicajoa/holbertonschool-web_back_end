80088fd053de40c8963d0eecca3b4418ddc6ff5a
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-19 17:54:17 -0500

    Add Empty session task, Create a class SessionAuth that inherits from Auth. For the moment this class will be empty, and Update app.py file for using SessionAuth instance for the variable auth depending of the value of the environment variable AUTH_TYPE

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Module of Session auth views
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Class SessionAuth that inherits from Auth
+
+    Args:
+        Auth ([Class]): [Parent class]
+    """
+    pass
