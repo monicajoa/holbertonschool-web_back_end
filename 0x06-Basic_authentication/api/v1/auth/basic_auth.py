53f026950675e705b4040ef2fd09d98814256934
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-18 21:52:36 -0500

    Add Basic - User object task, returns the User instance based on his email and password

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -61,3 +63,29 @@ class BasicAuth(Auth):
             return None, None
         data_spl_list = decoded_base64_authorization_header.split(':', 1)
         return (data_spl_list[0], data_spl_list[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Method Basic - User object,
+        Returns the User instance based
+        on his email and password
+        """
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+        try:
+            users_list = User.search({"email": user_email})
+        except Exception:
+            return None
+        for user in users_list:
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method Basic - Overload current_user,
+        overloads Auth and retrieves the User instance
+        for a request
+        """
