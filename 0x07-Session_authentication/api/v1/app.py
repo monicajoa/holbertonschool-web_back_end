0b71d0abfa46b6d0fd867c0f595781c3801dbb77
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-19 19:50:37 -0500

    Add Before request task, Update the @app.before_request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -51,13 +51,14 @@ def Forbidden(error) -> str:
 def before_request():
     """ Before_request handler, Filtering of each request
     """
-    new_list = ['/api/v1/status/',
-                '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    new_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth is None:
         return
     if auth.require_auth(request.path, new_list) is False:
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
