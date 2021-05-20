a7d9ae95c1d9b4e8491336c4bd0737ffb0254910
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-19 19:26:37 -0500

    Add Session cookie task, adding the method that returns a cookie value from a request

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -62,3 +63,11 @@ class Auth:
             [type]: [None]
         """
         return None
+
+    def session_cookie(self, request=None):
+        """[Method that returns a cookie value from a request]
+        """
+        if request is None:
+            return None
+        new_cookie_name = getenv('SESSION_NAME')
+        return request.cookies.get(new_cookie_name)
