9abeabeed08a0e38f99475946ec3a4ab428e090f
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-26 18:11:48 -0500

    Add update user task, update_user method that takes as argument a required user_id integer and arbitrary keyword arguments, and returns None, fix the conditional

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -66,7 +66,7 @@ class DB:
         user = self.find_user_by(id=user_id)
         try:
             for key, value in kwargs.items():
-                if key in user.__dict__:
+                if key in user.__dict__ and isinstance(value, type(user.__dict__[key])):
                     setattr(user, key, value)
                 else:
                     raise ValueError()
