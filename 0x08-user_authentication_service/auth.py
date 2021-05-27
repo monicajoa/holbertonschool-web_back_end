1da9f2525586f9e8eb0a0a920abb7288383352d5
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-26 21:49:40 -0500

    Add Credentials validation task, Try locating the user by email. If it exists, check the password with bcrypt.checkpw. If it matches return True. In any other case, return False

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -35,3 +35,16 @@ class Auth:
             hashed_password = _hash_password(password)
             register_user = self._db.add_user(email, hashed_password)
             return register_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Method Credentials validation, Try to reach the user by email,
+            It should expect required arguments by email and password and
+            Return a boolean
+        """
+        try:
+            find_user = self._db.find_user_by(email=email)
+            password = password.encode()
+            valid_login = bcrypt.checkpw(password, find_user.hashed_password)
+            return valid_login
+        except NoResultFound:
+            return False
