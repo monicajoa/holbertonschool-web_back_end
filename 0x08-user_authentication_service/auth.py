75ae7032844d3efdb47e90af514b30cc29f665f9
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-26 20:00:01 -0500

    Add Register user task, this method should take mandatory email and password string arguments and return a User object

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 """
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
@@ -12,3 +15,23 @@ def _hash_password(password: str) -> str:
     sal = bcrypt.gensalt()
     password = password.encode()
     return bcrypt.hashpw(password, sal)
+
+
+class Auth:
+    """ Auth class to interact with the authentication database
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method Register user, that takes email and password arguments,
+            Return a User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            register_user = self._db.add_user(email, hashed_password)
+            return register_user
