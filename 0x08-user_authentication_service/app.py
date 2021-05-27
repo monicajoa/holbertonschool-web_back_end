490a1999eac2eb824ec99c979e2cec423ec582b1
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-26 20:14:19 -0500

    Add basic flask app, Create a Flask app that has a single GET route and use flask.jsonify to return a JSON payload

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" [Module that holds Basic Flask app]
+"""
+
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def basic_flask_app(): -> str:
+    """ Method Basic Flask app, that create and set up
+        a basic Flask app, Return a JSON payload.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
