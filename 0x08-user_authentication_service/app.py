cd2b105a0922dbab24f804cb2682c89fe47d3910
monicajoa <mnortiz.ortiz@gmail.com>
2021-05-26 22:42:47 -0500

    Add Log in task, create a new session for the user, store it the session ID as a cookie with key session_id on the response and return a JSON payload of the form

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -31,5 +31,22 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ Method log in, responds to the POST / sessions path,
+        create a new session for the user, save the session id as a cookie,
+        Return a JSON payload of the form
+    """
+    email_data = request.form.get("email")
+    password_data = request.form.get("password")
+    if not AUTH.valid_login(email_data, password_data):
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email_data)
+        response = jsonify({"email": email_data, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
