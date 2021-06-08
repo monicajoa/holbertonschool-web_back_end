895f0c0cbc5c2961e75304b3e6d6854685469b1c
monicajoa <mnortiz.ortiz@gmail.com>
2021-06-07 23:59:23 -0500

    Add Writing strings to Redis task, store an instance of the Redis client as a private variable, Create a store method that takes a data argument and returns a string

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" [Module that holds writing strings to Redis]
+"""
+
+import redis
+from uuid import uuid4
+import typing
+
+
+class Cache:
+    """ Cache class
+    """
+
+    def __init__(self):
+        """ Constructor method,
+            store an instance of the
+            Redis client as a private variable
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        """ Method that generates a random key and stores
+            the input data also takes an argument (data)
+            and returns a string
+        """
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        return random_key
