46abc38298691394867ed97093a71960eb289785
monicajoa <mnortiz.ortiz@gmail.com>
2021-06-08 22:58:37 -0500

    Add  Retrieving lists task, function to display the history of calls of a particular function, change the variables

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -49,12 +49,12 @@ def replay(method: Callable):
         Method that holds a function to display the history
         of calls of a particular function
     """
-    self._redis = redis.Redis()
+    redis = redis.Redis()
     key = method.__qualname__
-    new_value = self._redis.get(key)
+    new_value = redis.get(key)
     decode_value = new_value.decode("utf-8")
-    new_inputs = self._redis.lrange(key + ':inputs', 0, -1)
-    new_outputs = self._redis.lrange(key + ':outputs', 0, -1)
+    new_inputs = redis.lrange(key + ':inputs', 0, -1)
+    new_outputs = redis.lrange(key + ':outputs', 0, -1)
     print('{} was called {} times:'.format(key, decode_value))
     for i, j in zip(new_inputs, new_outputs):
         print('{}(*{}) -> {}'.format(key, i.decode('utf-8'),
