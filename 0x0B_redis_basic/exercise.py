b16cf9a279997f9055e4acffaf0bdc4a82d5e583
monicajoa <mnortiz.ortiz@gmail.com>
2021-06-08 21:46:32 -0500

    Add  Storing lists task,  define a call_history decorator to store the history of inputs and outputs for a particular function

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,11 +17,30 @@ def count_calls(method: Callable) -> Callable:
 
     @wraps(method)
     def wrapper(self, *args, **kwds):
-        """ Method that contains a wrapped function,
+        """ Method that holds a wrapped function,
             which allows access to the Redis instance
         """
         self._redis.incr(key)
         return method(self, *args, **kwds)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """ (Storing lists),
+        Method that holds a decorator to store the history
+        of inputs and outputs for a particular function
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ Method that holds a wrapped function,
+            which allows access to the Redis instance
+        """
+        new_inputs = method.__qualname__ + ":inputs"
+        new_outputs = method.__qualname__ + ":outputs"
+        self._redis.rpush(new_inputs, str(args))
+        output_method = method(self, *args, **kwds)
+        self._redis.rpush(new_outputs, output_method)
+        return output_method
     return wrapper
 
 
@@ -38,6 +57,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Method that generates a random key and stores
             the input data also takes an argument (data)
