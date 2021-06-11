03599e848bebd32785f60dd6946cbbca5a163ceb
monicajoa <mnortiz.ortiz@gmail.com>
2021-06-10 20:16:21 -0500

    Add  We are all unique task, creates a table users with some parameters

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- We are all unique! (Task 0)
 -- Creates a table users with the attributes (id, email, name)
-CREATE table IF NOT EXIST users (
-    id PRIMARY KEY INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
