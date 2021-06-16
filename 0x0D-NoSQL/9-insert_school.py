42771caf9c252a6a768259949e3d4d9318b9a61c
monicajoa <mnortiz.ortiz@gmail.com>
2021-06-15 21:19:27 -0500

    Add Insert a document in Python task,  function that inserts a new document in a collection based on kwargs

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+""" Module that holds insert a document
+"""
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Method that inserts a new document in a
+        collection based on kwargs
+    """
+    new_document = mongo_collection.insert(kwargs)
+    return new_document
