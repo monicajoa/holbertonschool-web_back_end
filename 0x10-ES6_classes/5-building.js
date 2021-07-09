2a0ca6dcf85c512693a8b0c59a9a66274ba9cab1
monicajoa <mnortiz.ortiz@gmail.com>
2021-07-09 01:54:18 -0500

    Add  A Building task, implement a class named Building, Consider this class as an abstract class constructor and implement a method, fix the variable in the constructor

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   set sqft(sqft) {
